17bb1dae09d45625bd23a31477a2cec0d55dfa13
Динара Айкенова <@local.itstep>
2019-06-24 10:35:06 +0600

    Добавьте файлы проекта.

---
+++ 24.06.2019_Lesson/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._06._2019_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+            Console.WriteLine("Hello World!");// + endl
+
+            for (int i = 1; i < 9; i++)
+            {
+               Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+
+
+            Console.ReadKey();//pause
+        }
+    }
+}
